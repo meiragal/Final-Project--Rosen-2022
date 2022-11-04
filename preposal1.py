c967cc3ffc6e911fd18fa81fec22eac44ff32983
Meira Galapo <meiragalapo@Meiras-MBP.fios-router.home>
2022-11-03 23:46:54 -0400

    Adding python file

---
+++ preposal1.py
@@ -0,0 +1,32 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+Plan: 
+
+Our plan is to create a website where patients can enter their immunization records and be able to see which vaccines they need to get and when they need to get them (date). 
+
+The website will also provide a brief description of each vaccine and further information about where to get it/how. 
+
+Description: 
+For patients 18+ 
+
+Our pages on the vaccine forecasting website will include: 
+An information page on specific vaccines (i.e. facts, data, statistics) 
+A page where you can enter your immunization records and it will return the information of where and when to get the next vaccination
+
+What will happen:
+Patient will have to enter immunization information
+The computer  will convert immunization pdf to csv format 
+Then, the computer will return the vaccines that the patients are lacking and provide directions to the nearest pharmacy 
+
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+Keith Bunn, Kate Orlovskiy
+
+
+
+## Do you have any questions of your own?
+
+I don't think so! 
